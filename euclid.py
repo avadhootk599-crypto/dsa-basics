639fbe1d4f36287ea2d05b53b27249941c4dbd76
avadhootk599-crypto <avadhootk599@gmail.com>
2026-03-29 13:26:23 +0530

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -7,6 +7,15 @@ def euclid(x,y):
 
     return y
 
+def naive(x,y):
+    gcd=0
+    for i in range(1,min(x,y)+1):
+        if x%i==0 and y%i==0:
+            gcd=i
+    return gcd
 x=int(input("enter the value of first integer :"))
 y=int(input("enter the value of second integer :"))
+print("by euclid :")
 print(euclid(x,y))
+print("by naive")
+print(naive(x,y))
